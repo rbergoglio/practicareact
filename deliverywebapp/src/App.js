c7853e8b57e521002be4346979e78f17420e21c4
rbergoglio <ramiro.bergoglio@gmail.com>
2019-11-01 17:52:27 -0300

    dsds

--- deliverywebapp/src/App.js
+++ deliverywebapp/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <img src={"https://picsum.photos/200"} alt="" />
+
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
