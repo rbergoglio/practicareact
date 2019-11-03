99b4eedca1a200643b1d9bff3d5e0ab641684fc9
rbergoglio <ramiro.bergoglio@gmail.com>
2019-11-03 00:29:24 -0300

    holi

--- deliverywebapp/src/App.js
+++ deliverywebapp/src/App.js
@@ -1,29 +1,18 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <div className="blank-space-top"></div>
       <div>
         <img src={"https://picsum.photos/200"} alt="" />
 
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
