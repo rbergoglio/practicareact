555de6583a38b3b5f92ad0bf0e4a427bd555dd55
alu503 <alu503@labs>
2019-11-08 19:39:41 -0300

    grid

--- deliverywebapp/src/App.js
+++ deliverywebapp/src/App.js
@@ -6,12 +6,13 @@ import Carousel from "./components/Carousel";
 import BottomNavBar from "./components/BottomNavBar";
 import LocationInput from "./components/LocationInput";
 import FoodPictures from "./components/FoodPictures";
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-
+<Grid></Grid>
       <div className="blank-space-top"></div>
       <div>
         <LocationInput />
