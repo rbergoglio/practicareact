f0d58b8f0d3e3426cd4ade94ed54a08700e78db4
rbergoglio <ramiro.bergoglio@gmail.com>
2019-11-23 01:26:12 -0300

    leaflet

--- deliverywebapp/src/components/AboutUs.js
+++ deliverywebapp/src/components/AboutUs.js
@@ -1,8 +1,38 @@
 import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
-class AboutUs extends React.Component {
+export class AboutUs extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      lat: -34.613,
+      lng: -58.379,
+      zoom: 13
+    };
+  }
+
   render() {
-    return <div></div>;
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map
+        center={position}
+        zoom={this.state.zoom}
+        scrollWheelZoom={false}
+        style={{ height: "500px", width: "500px" }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </span>
+          </Popup>
+        </Marker>
+      </Map>
+    );
   }
 }
 
