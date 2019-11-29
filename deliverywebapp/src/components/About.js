7a0b59c5d95426084ea55d841d2e60fbf91814d2
rbergoglio <ramiro.bergoglio@gmail.com>
2019-11-29 04:58:22 -0300

    about

--- deliverywebapp/src/components/About.js
+++ deliverywebapp/src/components/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Row, Col } from "react-flexbox-grid";
+import Image from "react-bootstrap/Image";
 
 class About extends React.Component {
   render() {
@@ -12,9 +13,19 @@ class About extends React.Component {
             sm={6}
             md={8}
             lg={10}
-            style={{ backgroundColor: "white" }}
+            style={{
+              backgroundColor: "white"
+            }}
           >
-            aaa
+            <div>
+              <Image
+                src="https://images.pexels.com/photos/2438323/pexels-photo-2438323.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+                fluid
+                style={{
+                  maxWidth: "800px"
+                }}
+              />
+            </div>
           </Col>
           <Col xs={12} sm={3} md={2} lg={1} />
         </Row>
